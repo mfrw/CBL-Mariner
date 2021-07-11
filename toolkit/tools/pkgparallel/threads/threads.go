1a25fb0887f8cf886a8f3e09849f06e34ddab56f
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2021-07-11 15:27:44 +0530

    threads
    Signed-off-by: Muhammad Falak Wani <mwani@microsoft.com>

--- toolkit/tools/pkgparallel/threads/threads.go
+++ toolkit/tools/pkgparallel/threads/threads.go
@@ -1,7 +1,21 @@
+// Go program to illustrate send
+// and receive operation
 package main
+
 import (
 	"fmt"
+	"microsoft.com/pkggen/internal/pkggraph"
 )
-func main(){
-	
+
+func myfunc(ch chan int) {
+	fmt.Println(234 + <-ch)
+}
+func main() {
+	fmt.Println("start Main method")
+	// Creating a channel
+	ch := make(chan int)
+
+	go myfunc(ch)
+	ch <- 23
+	fmt.Println("End Main method")
 }
