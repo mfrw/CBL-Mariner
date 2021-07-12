e7b8a64e6eb7f6eb950a97e941167eee5521ccf4
Rakshaa Viswanathan <rakshaa2000@gmail.com>
2021-07-12 08:40:51 +0530

    trying threads
    Signed-off-by: Muhammad Falak Wani <mwani@microsoft.com>

--- toolkit/tools/pkgparallel/threads/threads.go
+++ toolkit/tools/pkgparallel/threads/threads.go
@@ -4,19 +4,26 @@ package main
 
 import (
 	"fmt"
-	"gonum.org/v1/gonum/graph"
-	"gonum.org/v1/gonum/graph/path"
 	"microsoft.com/pkggen/internal/pkggraph"
-	"microsoft.com/pkggen/pkgparallel/unravel"
+	"microsoft.com/pkggen/pkgparallel/clustering"
 )
 
-func myfunc(ch chan int) {
+func myfunc(ch chan graph.Node) {
 	fmt.Println(234 + <-ch)
 }
 func main() {
-	ch := make(chan map[graph.Node]graph.Node)
-
-	go myfunc(ch)
-	ch <- 23
-	fmt.Println("End Main method")
+	ch := make(chan graph.Node)
+	file := "./cdrkit.dot"
+	g := pkggraph.NewPkgGraph()
+	err := pkggraph.ReadDOTGraphFile(g, file)
+	leader := Clusterise.Clusterise(g)
+	lists:= make(map[graph.Node][]graph.Node)
+	for key, element in range leader{
+		lists[element] = append(lists[element],key)
+	}
+	for key, element := range lists{
+		for i in range element{
+			ch<-i
+		}
+	}
 }
